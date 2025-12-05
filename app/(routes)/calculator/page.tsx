9ed497030b4ed2ce4f29cbac87c2cb5e5f543b9b
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-12-05 17:07:39 +0800

    feat:create calculator page

---
+++ app/(routes)/calculator/page.tsx
@@ -0,0 +1,29 @@
+import Content from "@/app/_components/calculator/content";
+import Footer from "@/app/_components/footer";
+import Menu from "@/app/_components/menu";
+import Navbar from "@/app/_components/navbar";
+
+export default function CalculatorPage() {
+    return (
+            <>
+                <div className="min-h-screen bg-slate-50">
+                    <div className="bg-white shadow-sm border-b border-slate-200">
+                        <Navbar />
+                    </div>
+    
+                    <div className="bg-black">
+                        <Menu />
+                    </div>
+    
+                    <div>
+                        <main className="max-w-7xl mx-auto p-2">
+                            <Content />
+                        </main>
+                    </div>
+    
+                </div>
+                <Footer />
+            </>
+    
+        );
+}
