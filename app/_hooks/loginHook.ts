0187edb8a216fe501ff83cd2ffe5378341a20533
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 13:38:12 +0800

    feat:finish authentication

--- app/_hooks/loginHook.ts
+++ app/_hooks/loginHook.ts
@@ -7,13 +7,10 @@ import { loginService } from "../_services/login";
 import { toast } from "sonner";
 import axios from "axios";
 import { ServerError } from "../_types/errorType";
-import { useDispatch } from "react-redux";
-import { setlogin, setlogout } from "../_stores/authSlice";
 
 export default function LoginHook() {
     const { register, handleSubmit, formState: { errors }, setError } = useForm<LoginFormData>();
     const [showPassword, setShowPassword] = useState(false);
-    const dispatch = useDispatch();
 
     const onSubmit = async (data: LoginFormData) => {
         try {
@@ -22,12 +19,10 @@ export default function LoginHook() {
 
             if (response.status === 'success') {
                 toast.success("Login berhasil!");
-                dispatch(setlogin(response.data));
                 window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/app/dashboard`;
             }
             return response;
         } catch (error: unknown) {
-            dispatch(setlogout());
             if (axios.isAxiosError<ServerError>(error)) {
                 const status = error.status;
 
