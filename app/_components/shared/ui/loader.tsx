6ec8006fbd575271b7e41bfc6c19c9a81d1e2b38
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 13:38:06 +0800

    feat:finish authentication

---
+++ app/_components/shared/ui/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="translate animate-spin rounded-full border-8 border-solid border-red-600 border-t-transparent h-16 w-16"></div>
+        </div>
+    );
+}
