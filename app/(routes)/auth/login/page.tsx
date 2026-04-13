c9bd57ee54ab3370bb67c654c9ec80560d4f2522
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:29:00 +0800

    feat:handle authentication used cookie method

--- app/(routes)/auth/login/page.tsx
+++ app/(routes)/auth/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import LoginHook from "@/app/_hooks/loginHook";
 import Link from "next/link";
 import { BsEye } from "react-icons/bs";
 import { CiCalculator1 } from "react-icons/ci";
@@ -7,6 +8,7 @@ import { FaInfoCircle } from "react-icons/fa";
 
 
 export default function RegisterPage() {
+    const { register, handleSubmit, errors, onSubmit, showPassword, setShowPassword } = LoginHook();
     return (
         <div className="bg-slate-50">
             <div className="flex flex-col justify-center items-center space-x-2 h-screen">
@@ -20,13 +22,13 @@ export default function RegisterPage() {
                             Hitung HPP & Keuntungan dengan mudah
                         </span>
                     </div>
-                    <form className="flex flex-col space-y-2 w-md p-4">
-                        {/* {errors.root?.serverError && (
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-md p-4">
+                        {errors.root?.serverError && (
                             <div className="flex items-center space-x-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
                                 <FaInfoCircle size={20} className="text-red-500" />
                                 <span>{errors.root.serverError.message}</span>
                             </div>
-                        )} */}
+                        )}
                         <div>
                             <label htmlFor="">Email</label>
                             <input type="email" {...register('email', { required: "Email wajib diisi", pattern: { value: /^\S+@\S+$/i, message: "Format email tidak valid" } })} id="email" className={`border p-2 rounded-lg w-full placeholder:text-sm ${errors.email ? 'border-red-500' : ''}`} placeholder="Masukkan Email" />
@@ -37,7 +39,7 @@ export default function RegisterPage() {
                             <label htmlFor="">Password</label>
                             <div className="relative">
                                 <input type="password" {...register('password', { required: "Password wajib diisi", minLength: { value: 6, message: "Password minimal 6 karakter" } })} id="password" className={`border p-2 rounded-lg w-full placeholder:text-sm ${errors.password ? 'border-red-500' : ''}`} placeholder="Masukkan Password" />
-                                <BsEye size={20} className="text-black absolute right-4 top-3 cursor-pointer" />
+                                <BsEye size={20} className="text-black absolute right-4 top-3 cursor-pointer" onClick={() => setShowPassword(!showPassword)} />
                             </div>
                             {errors.password && <div className="flex items-center space-x-2 mt-2"><FaInfoCircle size={15} className="text-red-500" /> <span className="text-red-500 text-sm">{errors.password.message}</span></div>}
                         </div>
