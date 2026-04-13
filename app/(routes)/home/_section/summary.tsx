103fec0d0ea426a8fc2d498f4172fca7ca5a75b1
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:29:13 +0800

    feat:handle authentication used cookie method

---
+++ app/(routes)/home/_section/summary.tsx
@@ -0,0 +1,32 @@
+export default function Summary() {
+    return (
+        <section className="py-5 border-b border-slate-200">
+            <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-6 gap-8 text-center">
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">1.284+</h2>
+                    <p className="text-gray-500 mt-2">Pelaku Usaha</p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">98%</h2>
+                    <p className="text-gray-500 mt-2">Akurasi Perhitungan</p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">5x</h2>
+                    <p className="text-gray-500 mt-2">Lebih Cepat dari Manual</p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">24/7</h2>
+                    <p className="text-gray-500 mt-2">Dukungan Pelanggan</p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">Rp 500jt+</h2>
+                    <p className="text-gray-500 mt-2">Penghematan Biaya</p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold text-gray-900">4.8/5</h2>
+                    <p className="text-gray-500 mt-2">Rating Pengguna</p>
+                </div>
+            </div>
+        </section>
+    )
+}
