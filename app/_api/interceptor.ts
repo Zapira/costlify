f90a564e3029c475153549d5a6d038d44776c8f4
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:29:15 +0800

    feat:handle authentication used cookie method

--- app/_api/interceptor.ts
+++ app/_api/interceptor.ts
@@ -1,29 +1,23 @@
+'use client';
+
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = document.cookie.match(/(^| )access_token=([^;]+)/);
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token[2]}`;
-        }
-
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
 api.interceptors.response.use(
     (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            window.location.href = "/auth/login";
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            useRouter().push("/auth/login"); 
         }
+
         return Promise.reject(error);
     }
 );
