5cbbf12abb92e984da1ad802d383e0f15b289337
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 13:36:46 +0800

    feat:finish authentication

--- app/(routes)/app/dashboard/page.tsx
+++ app/(routes)/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
-import Content from "@/app/_components/dashboard/content";
-import Footer from "@/app/_components/footer";
-import Menu from "@/app/_components/menu";
-import Navbar from "@/app/_components/navbar";
+'use client'
+
+import Footer from "@/app/_components/shared/footer";
+import Menu from "@/app/_components/shared/menu";
+import Navbar from "@/app/_components/shared/navbar";
+import CardInformation from "./_section/cardInformation";
+import TableRingkasan from "./_section/tableRingkasan";
+import DashboardHook from "@/app/_hooks/dashboardHook";
+import { useSelector } from "react-redux";
+import Loader from "@/app/_components/shared/ui/loader";
 
 export default function Dashboard() {
+    DashboardHook();
+    const loading = useSelector((state: any) => state.auth.loading);
+
+    if (loading) {
+        return (
+            <Loader />
+        )
+    }
     return (
         <>
             <div className="min-h-screen bg-slate-50">
@@ -16,9 +30,10 @@ export default function Dashboard() {
                 </div>
 
                 <div>
-                    {/* <main className="max-w-7xl mx-auto p-2">
-                        <Content />
-                    </main> */}
+                    <main className="max-w-7xl mx-auto p-2">
+                        <CardInformation />
+                        <TableRingkasan />
+                    </main>
                 </div>
 
             </div>
