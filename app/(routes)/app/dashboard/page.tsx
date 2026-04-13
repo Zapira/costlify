0502a80f267347f44021711040300cd6e6d5009a
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:28:56 +0800

    feat:handle authentication used cookie method

--- app/(routes)/app/dashboard/page.tsx
+++ app/(routes)/app/dashboard/page.tsx
@@ -1,17 +1,11 @@
-'use client'
-
-import AuthChecking from "@/app/_components/auth/authChecking";
 import Content from "@/app/_components/dashboard/content";
 import Footer from "@/app/_components/footer";
 import Menu from "@/app/_components/menu";
 import Navbar from "@/app/_components/navbar";
 
-
-
 export default function Dashboard() {
     return (
         <>
-            <AuthChecking />
             <div className="min-h-screen bg-slate-50">
                 <div className="bg-white shadow-sm border-b border-slate-200">
                     <Navbar />
@@ -22,9 +16,9 @@ export default function Dashboard() {
                 </div>
 
                 <div>
-                    <main className="max-w-7xl mx-auto p-2">
+                    {/* <main className="max-w-7xl mx-auto p-2">
                         <Content />
-                    </main>
+                    </main> */}
                 </div>
 
             </div>
