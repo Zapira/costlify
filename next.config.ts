19f5906d52de32a8a51d0a84dcd6924e00f7fa70
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:28:18 +0800

    feat:cahnge npm to bun

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/produk-saya",
-        destination: "/product",
-      },
-      {
-        source: "/kalkulator-hpp",
-        destination: "/calculator",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
