8d159af27649194e5fc8b8c90dc1d2c066d913db
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-13 12:28:43 +0800

    feat:handle authentication used cookie method

--- proxy.ts
+++ proxy.ts
@@ -1,32 +1,45 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function proxy(req: NextRequest) {
-    console.log('Proxy request received:', req.url);
     const token = req.cookies.get("access_token")?.value;
-    if(!token){
-        return NextResponse.redirect(new URL("/", req.url));
+    const pathname = req.nextUrl.pathname;
+
+    const isAuthPage = pathname.startsWith("/auth");
+    const isProtectedPage = pathname.startsWith("/app");
+
+    if (!token) {
+        if (isProtectedPage) {
+            return NextResponse.redirect(new URL("/auth/login", req.url));
+        }
+        return NextResponse.next();
     }
 
-    const authChecking = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth-check`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/auth-check`, {
+        method: "GET",
         headers: {
-            Authorization: `Bearer ${token}`,
+            Cookie: `access_token=${token}`,
         },
     });
-    const authCheckingData = await authChecking.json();
-    console.log('auth checking:', authCheckingData);
 
-    if (authCheckingData.message === 'unauthorized') {
-        return NextResponse.redirect(new URL("/", req.url));
-    } else {
-        return NextResponse.next()
+    const isValid = res.status === 200;
+
+    if (!isValid) {
+        if (isProtectedPage) {
+            return NextResponse.redirect(new URL("/auth/login", req.url));
+        }
+        return NextResponse.next();
     }
+
+    if (isAuthPage) {
+        return NextResponse.redirect(new URL("/app/dashboard", req.url));
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {
     matcher: [
-        "/app/dashboard",
-        "/app/produk-saya",
-        "/app/produk-saya/:path*",
+        "/app/:path*",
+        "/auth/:path*",
     ],
 };
