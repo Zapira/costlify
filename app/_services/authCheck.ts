4d6fb17c16f3367bff36d8dcfbd28d37fa983cbe
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-14 14:14:02 +0800

    feat:update home page

--- app/_services/authCheck.ts
+++ app/_services/authCheck.ts
@@ -1,17 +1,11 @@
 import api from "../_api/interceptor"
-import { setLoading, setlogin, setlogout } from "../_stores/authSlice";
-import { store } from "../_stores/store";
 
 export const AuthChecking = async () => {
     try {
         const response = await api.get("/v1/profile");
-        store.dispatch(setlogin(response.data));
+        return response.data;
 
     } catch (error) {
-        store.dispatch(setlogout());
-    } finally {
-        setTimeout(() => {
-            store.dispatch(setLoading(false));
-        }, 1000);
+        console.error("Auth check failed:", error);
     }
 };
