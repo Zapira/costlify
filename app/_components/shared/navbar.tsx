acb5f72eef90b18d4aee40a983d115bccbe7dea5
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-14 14:55:23 +0800

    feat:add new section in home

--- app/_components/shared/navbar.tsx
+++ app/_components/shared/navbar.tsx
@@ -4,17 +4,12 @@ import Link from "next/link";
 import { BiMenu } from "react-icons/bi";
 import { CiCalculator1 } from "react-icons/ci";
 import { LuBox, LuCalculator, LuLayoutDashboard } from "react-icons/lu";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { CgLogOut, CgProfile } from "react-icons/cg";
-import { FaArrowAltCircleRight } from "react-icons/fa";
 import useDashboardHook from "@/app/_hooks/dashboardHook";
-import Loader from "./ui/loader";
-import AuthContext from "@/app/_contexts/authContext";
-
 
 export default function Navbar() {
     const [openNavbar, setOpenNavbar] = useState(false);
-    const {isLoggedIn, user, loading} = useContext(AuthContext);
 
     const menuItems = [
         { href: "/app/dashboard", icon: <LuLayoutDashboard />, label: "Dashboard" },
@@ -27,6 +22,7 @@ export default function Navbar() {
     };
     const { handlerLogout } = useDashboardHook();
 
+  
 
     return (
         <div className="max-w-7xl mx-auto px-4">
@@ -44,30 +40,12 @@ export default function Navbar() {
                 </div>
 
                 <div className="flex items-center space-x-3">
-                    {isLoggedIn ? (
-                        <button onClick={handlerLogout}
-                            className="flex items-center space-x-3 px-4 py-2 text-black rounded-lg hover:bg-slate-100 border-2 border-slate-300 hover:border-slate-400 transition-colors"
-                        >
-                            <CgLogOut size={24} className="text-black" />
-                            <span>Keluar</span>
-                        </button>
-                    ) : (
-                        <>
-                            <Link
-                                href="/auth/login   "
-                                className="px-5 py-2 font-medium text-slate-600 hover:text-black hover:bg-slate-100 rounded-lg transition-colors"
-                            >
-                                Masuk
-                            </Link>
-                            <Link
-                                href="/auth/register"
-                                className="px-6 py-2 font-medium bg-linear-to-r from-sky-500 to-emerald-500 text-white rounded-xl transition-colors flex items-center justify-center space-x-2 hover:bg-linear-to-r hover:from-sky-600 hover:to-emerald-600"
-                            >
-                                <span className="font-bold">Coba Gratis</span>
-                                <FaArrowAltCircleRight size={15} />
-                            </Link>
-                        </>
-                    )}
+                    <button onClick={handlerLogout}
+                        className="flex items-center space-x-3 px-4 py-2 text-black rounded-lg hover:bg-slate-100 border-2 border-slate-300 hover:border-slate-400 transition-colors"
+                    >
+                        <CgLogOut size={24} className="text-black" />
+                        <span>Keluar</span>
+                    </button>
 
                 </div>
             </nav>
