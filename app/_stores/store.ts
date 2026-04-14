a5ecaf2e7fe1c489a30fea3b6851ef5611d59cb8
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-14 14:14:12 +0800

    feat:update home page

--- app/_stores/store.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReduce from "./authSlice"
-export const store = configureStore({
-    reducer: {
-        auth: authReduce,
-    },
-})
