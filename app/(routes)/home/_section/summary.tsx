9427a080c93e70e55ea550a2aaba2a2dba8194b6
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-14 14:13:41 +0800

    feat:update home page

--- app/(routes)/home/_section/summary.tsx
+++ app/(routes)/home/_section/summary.tsx
@@ -1,6 +1,6 @@
 export default function Summary() {
     return (
-        <section className="py-5 border-b border-slate-200">
+        <section className="py-5 border-b bg-white border-slate-200">
             <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-6 gap-8 text-center">
                 <div>
                     <h2 className="text-3xl font-bold text-gray-900">1.284+</h2>
