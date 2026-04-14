9d36af2380d872880c0947d7e903d2fbc0182362
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-14 14:33:05 +0800

    feat:update

--- app/(routes)/home/index.tsx
+++ app/(routes)/home/index.tsx
@@ -1,9 +1,8 @@
 import Footer from "@/app/_components/shared/footer";
+import Navbar from "@/app/_components/shared/navbar";
 import Hero from "./_section/hero";
 import Summary from "./_section/summary";
 import BestFeature from "./_section/bestFeature";
-import NavbarHome from "@/app/_components/shared/navbarHome";
-import ExampleListHppProduct from "./_section/exampleListHppProduct";
 
 
 export default function Index() {
@@ -11,13 +10,12 @@ export default function Index() {
         <>
             <div className="min-h-screen bg-slate-50">
                 <div className="bg-white shadow-sm border-b border-slate-200">
-                    <NavbarHome />
+                    <Navbar />
                 </div>
                 <div>
                     <Hero />
                     <Summary />
                     <BestFeature />
-                    <ExampleListHppProduct />
                     <main className="max-w-7xl mx-auto p-2">
 
                     </main>
